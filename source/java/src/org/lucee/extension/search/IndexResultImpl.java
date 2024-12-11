e8a36dce08017d10b864c4bfc74a72608c96e7ee
michaeloffner <michaeloffner@users.noreply.github.com>
2024-12-11 13:23:37 +0100

    update project to use maven build

--- source/java/src/org/lucee/extension/search/IndexResultImpl.java
+++ source/java/src/org/lucee/extension/search/IndexResultImpl.java
@@ -3,18 +3,19 @@ package org.lucee.extension.search;
 import lucee.runtime.search.IndexResult;
 
 public final class IndexResultImpl implements IndexResult {
-	
+
 	private int countDeleted;
 	private int countInserted;
 	private int countUpdated;
-	
-	public static IndexResult EMPTY=new IndexResultImpl(0,0,0);
 
-	public IndexResultImpl(int countDeleted,int countInserted,int countUpdated) {
-		this.countDeleted=countDeleted;
-		this.countInserted=countInserted;
-		this.countUpdated=countUpdated;
+	public static IndexResult EMPTY = new IndexResultImpl(0, 0, 0);
+
+	public IndexResultImpl(int countDeleted, int countInserted, int countUpdated) {
+		this.countDeleted = countDeleted;
+		this.countInserted = countInserted;
+		this.countUpdated = countUpdated;
 	}
+
 	public IndexResultImpl() {
 	}
 
@@ -34,44 +35,51 @@ public final class IndexResultImpl implements IndexResult {
 	}
 
 	/**
-	 * @param countDeleted the countDeleted to set
+	 * @param countDeleted
+	 *            the countDeleted to set
 	 */
 	public void setCountDeleted(int countDeleted) {
 		this.countDeleted = countDeleted;
 	}
 
 	/**
-	 * @param countInserted the countInserted to set
+	 * @param countInserted
+	 *            the countInserted to set
 	 */
 	public void setCountInserted(int countInserted) {
 		this.countInserted = countInserted;
 	}
 
 	/**
-	 * @param countUpdated the countUpdated to set
+	 * @param countUpdated
+	 *            the countUpdated to set
 	 */
 	public void setCountUpdated(int countUpdated) {
 		this.countUpdated = countUpdated;
 	}
+
 	/**
-	 * @param countDeleted the countDeleted to set
+	 * @param countDeleted
+	 *            the countDeleted to set
 	 */
 	public void incCountDeleted() {
 		this.countDeleted++;
 	}
 
 	/**
-	 * @param countInserted the countInserted to set
+	 * @param countInserted
+	 *            the countInserted to set
 	 */
 	public void incCountInserted() {
 		this.countInserted++;
 	}
 
 	/**
-	 * @param countUpdated the countUpdated to set
+	 * @param countUpdated
+	 *            the countUpdated to set
 	 */
 	public void incCountUpdated() {
 		this.countUpdated++;
 	}
-	
+
 }
