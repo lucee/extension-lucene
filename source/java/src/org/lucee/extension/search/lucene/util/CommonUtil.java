126ea17d93aa5b173e77a814d32b417968bc4b9c
michaeloffner <michaeloffner@users.noreply.github.com>
2025-01-17 16:52:12 +0100

    update to 7.7.3

--- source/java/src/org/lucee/extension/search/lucene/util/CommonUtil.java
+++ source/java/src/org/lucee/extension/search/lucene/util/CommonUtil.java
@@ -2,10 +2,7 @@ package org.lucee.extension.search.lucene.util;
 
 import java.io.Closeable;
 
-import org.apache.lucene.util.Version;
-
 public class CommonUtil {
-	public static final Version VERSION = Version.LUCENE_5_5_5;
 
 	public static void closeSilently(Closeable closable) {
 		if (closable != null) {
