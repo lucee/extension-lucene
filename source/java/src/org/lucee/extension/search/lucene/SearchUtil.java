854a2ec362172d51bbb59e1f35b30ef0aff67d7a
michaeloffner <michaeloffner@users.noreply.github.com>
2025-01-17 09:33:28 +0100

    update to 3.6.2

--- source/java/src/org/lucee/extension/search/lucene/SearchUtil.java
+++ source/java/src/org/lucee/extension/search/lucene/SearchUtil.java
@@ -8,102 +8,93 @@ import org.apache.lucene.analysis.br.BrazilianAnalyzer;
 import org.apache.lucene.analysis.cjk.CJKAnalyzer;
 import org.apache.lucene.analysis.cn.ChineseAnalyzer;
 import org.apache.lucene.analysis.cz.CzechAnalyzer;
+import org.apache.lucene.analysis.da.DanishAnalyzer;
 import org.apache.lucene.analysis.de.GermanAnalyzer;
 import org.apache.lucene.analysis.el.GreekAnalyzer;
+import org.apache.lucene.analysis.es.SpanishAnalyzer;
+import org.apache.lucene.analysis.fi.FinnishAnalyzer;
 import org.apache.lucene.analysis.fr.FrenchAnalyzer;
+import org.apache.lucene.analysis.hu.HungarianAnalyzer;
+import org.apache.lucene.analysis.it.ItalianAnalyzer;
 import org.apache.lucene.analysis.nl.DutchAnalyzer;
+import org.apache.lucene.analysis.no.NorwegianAnalyzer;
+import org.apache.lucene.analysis.pt.PortugueseAnalyzer;
 import org.apache.lucene.analysis.ru.RussianAnalyzer;
-import org.apache.lucene.analysis.snowball.SnowballAnalyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.analysis.standard.StandardTokenizer;
+import org.apache.lucene.analysis.sv.SwedishAnalyzer;
 import org.apache.lucene.analysis.th.ThaiAnalyzer;
+import org.apache.lucene.analysis.tr.TurkishAnalyzer;
+import org.lucee.extension.search.lucene.util.CommonUtil;
 
-import lucee.loader.engine.CFMLEngine;
-import lucee.loader.engine.CFMLEngineFactory;
 import lucee.runtime.search.SearchException;
 
-import org.lucee.extension.search.lucene.analyzer.DanishAnalyzer;
-import org.lucee.extension.search.lucene.analyzer.ItalianAnalyzer;
-import org.lucee.extension.search.lucene.analyzer.NorwegianAnalyzer;
-import org.lucee.extension.search.lucene.analyzer.PortugueseAnalyzer;
-import org.lucee.extension.search.lucene.analyzer.SpanishAnalyzer;
-import org.tartarus.snowball.ext.FinnishStemmer;
-import org.tartarus.snowball.ext.HungarianStemmer;
-import org.tartarus.snowball.ext.TurkishStemmer;
 public final class SearchUtil {
 
-	private static Map<String,Analyzer> analyzers=new ConcurrentHashMap<>();
-	
+	private static Map<String, Analyzer> analyzers = new ConcurrentHashMap<>();
+
 	public static Analyzer getAnalyzer(String language) throws SearchException {
-		
-        if(language==null)language="english";
-        else language=language.toLowerCase().trim();
-        language=org.lucee.extension.search.SearchUtil.translateLanguage(language);
-		
-        Analyzer analyzer=analyzers.get(language);
-        if(analyzer!=null) return analyzer;
-        StandardTokenizer st=null;
-        
-        if(language.equals("english"))
-        	analyzer= new StandardAnalyzer();
-        else if(language.equals("german")) 		
-        	analyzer= new GermanAnalyzer();
-        else if(language.equals("russian")) 	
-        	analyzer= new RussianAnalyzer();
-        else if(language.equals("dutch")) 		
-        	analyzer= new DutchAnalyzer();
-        else if(language.equals("french")) 		
-        	analyzer= new FrenchAnalyzer();
-        else if(language.equals("norwegian")) 	
-        	analyzer= new NorwegianAnalyzer();
-        else if(language.equals("portuguese")) 	
-        	analyzer= new PortugueseAnalyzer();
-        else if(language.equals("spanish")) 	
-        	analyzer= new SpanishAnalyzer();
-        else if(language.equals("brazilian")) 	
-        	analyzer= new BrazilianAnalyzer();
-        else if(language.equals("chinese")) 	
-        	analyzer= new ChineseAnalyzer();
-        else if(language.startsWith("czech")) 	
-        	analyzer= new CzechAnalyzer();
-        else if(language.equals("greek")) 		
-        	analyzer= new GreekAnalyzer();
-        else if(language.equals("thai"))
-        	analyzer= new ThaiAnalyzer();
-        else if(language.equals("japanese"))
-        	analyzer= new CJKAnalyzer();
-        else if(language.equals("korean"))
-        	analyzer= new CJKAnalyzer();
-        else if(language.equals("italian"))
-        	analyzer= new ItalianAnalyzer();
-        else if(language.equals("danish"))
-        	analyzer= new DanishAnalyzer();
-        else if(language.equals("norwegian"))
-        	analyzer= new NorwegianAnalyzer();
-        else if(language.equals("finnish"))
-        	analyzer=new SnowballAnalyzer( "Finnish" );
-        else if(language.equals("swedish"))
-        	analyzer= new SnowballAnalyzer( "Swedish" );
-        else if(language.equals("hungarian"))
-        	analyzer= new SnowballAnalyzer( "Hungarian" );
-        else if(language.equals("turkish"))
-        	analyzer= new SnowballAnalyzer( "Turkish" );
-        
-        else {
-        	CFMLEngine engine = CFMLEngineFactory.getInstance();
-        	String clazzName="org.apache.lucene.analysis.el."+engine.getStringUtil().ucFirst(language.trim().toLowerCase())+"Analyzer;";
-        	Object o=engine.getClassUtil().loadInstance(clazzName,(Object)null);
-            if(o==null){
-            	clazzName="org.opencfmlfoundation.search.lucene.analyzer."+engine.getStringUtil().ucFirst(language.trim().toLowerCase())+"Analyzer";
-            	o=engine.getClassUtil().loadInstance(clazzName,(Object)null);//Class.orName(clazzName).newInstance();
-        	}
-            if(o instanceof Analyzer) analyzer=(Analyzer) o;
-            else if(o==null) 
-            	 throw new SearchException("No Language Analyzer for Lanuage "+language+" found");
-            else 
-                throw new SearchException( "can't create Language Analyzer for Lanuage "+language+", Analyzer ["+clazzName+"] is of invalid type");
-        }        
-        analyzers.put(language, analyzer);
-        return analyzer;
-    }
+
+		if (language == null)
+			language = "english";
+		else
+			language = language.toLowerCase().trim();
+		language = org.lucee.extension.search.SearchUtil.translateLanguage(language);
+
+		Analyzer analyzer = analyzers.get(language);
+		if (analyzer != null)
+			return analyzer;
+		StandardTokenizer st = null;
+
+		if (language.equals("english"))
+			analyzer = new StandardAnalyzer(CommonUtil.VERSION);
+		else if (language.equals("german"))
+			analyzer = new GermanAnalyzer(CommonUtil.VERSION);
+		else if (language.equals("russian"))
+			analyzer = new RussianAnalyzer(CommonUtil.VERSION);
+		else if (language.equals("dutch"))
+			analyzer = new DutchAnalyzer(CommonUtil.VERSION);
+		else if (language.equals("french"))
+			analyzer = new FrenchAnalyzer(CommonUtil.VERSION);
+		else if (language.equals("norwegian"))
+			analyzer = new NorwegianAnalyzer(CommonUtil.VERSION);
+		else if (language.equals("portuguese"))
+			analyzer = new PortugueseAnalyzer(CommonUtil.VERSION);
+		else if (language.equals("spanish"))
+			analyzer = new SpanishAnalyzer(CommonUtil.VERSION);
+		else if (language.equals("brazilian"))
+			analyzer = new BrazilianAnalyzer(CommonUtil.VERSION);
+		else if (language.equals("chinese"))
+			analyzer = new ChineseAnalyzer();
+		else if (language.startsWith("czech"))
+			analyzer = new CzechAnalyzer(CommonUtil.VERSION);
+		else if (language.equals("greek"))
+			analyzer = new GreekAnalyzer(CommonUtil.VERSION);
+		else if (language.equals("thai"))
+			analyzer = new ThaiAnalyzer(CommonUtil.VERSION);
+		else if (language.equals("japanese"))
+			analyzer = new CJKAnalyzer(CommonUtil.VERSION);
+		else if (language.equals("korean"))
+			analyzer = new CJKAnalyzer(CommonUtil.VERSION);
+		else if (language.equals("italian"))
+			analyzer = new ItalianAnalyzer(CommonUtil.VERSION);
+		else if (language.equals("danish"))
+			analyzer = new DanishAnalyzer(CommonUtil.VERSION);
+		else if (language.equals("norwegian"))
+			analyzer = new NorwegianAnalyzer(CommonUtil.VERSION);
+		else if (language.equals("finnish"))
+			analyzer = new FinnishAnalyzer(CommonUtil.VERSION);
+		else if (language.equals("swedish"))
+			analyzer = new SwedishAnalyzer(CommonUtil.VERSION);
+		else if (language.equals("hungarian"))
+			analyzer = new HungarianAnalyzer(CommonUtil.VERSION);
+		else if (language.equals("turkish"))
+			analyzer = new TurkishAnalyzer(CommonUtil.VERSION);
+
+		else {// TODO add more
+			throw new SearchException("No Language Analyzer for Lanuage " + language + " found");
+		}
+		analyzers.put(language, analyzer);
+		return analyzer;
+	}
 }
---
+++ source/java/src/org/lucee/extension/search/lucene/util/CommonUtil.java
@@ -0,0 +1,22 @@
+package org.lucee.extension.search.lucene.util;
+
+import java.io.Closeable;
+
+import org.apache.lucene.util.Version;
+
+public class CommonUtil {
+	public static final Version VERSION = Version.LUCENE_36;
+
+	public static void closeSilently(Closeable closable) {
+		if (closable != null) {
+			try {
+				closable.close();
+
+			} catch (Exception e) {
+
+			}
+		}
+
+	}
+
+}
